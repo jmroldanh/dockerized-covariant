f9101d3eccd5aaadc491abcc3a1333775cc3e1e1
Jose Manuel Roldan Herraiz <josemauel.roldan@a3sec.com>
2020-03-23 00:58:58 +0100

    trying to fix creation of security group not being idempotent

--- main.tf
+++ main.tf
@@ -2,12 +2,17 @@ provider "aws" {
   region = "eu-west-3"
 }
 
-resource "aws_instance" "covariant" {
-  ami           = "ami-0a08edebc55d3bb93"
-  instance_type = "t2.micro"
+data "aws_vpc" "default" {
+  default = true
 }
+data "aws_security_group" "default" {
+  name   = "default"
+  vpc_id = data.aws_vpc.default.id
+}
+
 resource "aws_security_group" "covariant" {
   name = "terraform-example-instance"
+  vpc_id      = data.aws_vpc.default.id
 
   ingress {
     from_port   = 80
@@ -15,4 +20,10 @@ resource "aws_security_group" "covariant" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+resource "aws_instance" "covariant" {
+  ami                     = "ami-0a08edebc55d3bb93"
+  instance_type           = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.covariant.id]
 }
